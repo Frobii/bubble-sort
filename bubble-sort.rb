a915fc3568bcf0d7aa2ef27e37ad6d7e47f1f104
Frobi <frobi.webdev@outlook.com>
2022-07-14 10:09:00 +0930

    create-bubble-sort-method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,1 +1,16 @@
+def bubble_sort(array)
+    swap_count = 0
+    
+    until swap_count == (array.length - 1)
+        array.each_with_index do |number, index|
+            if array[index + 1] != nil && number > array[index + 1]
+                array.insert(index, array.delete_at(index + 1))
+            end
+        end
+        swap_count += 1
+    end
 
+    p array
+end
+
+bubble_sort([4,3,78,2,0,2])
